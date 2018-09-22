55b5ee15731029d8e0f44625ebd4cd0ae1304f86
TheScarastic <warabhishek@gmail.com>
2018-09-22 04:20:56 -0400

    beryllium: Add device input configs and keylayouts
    Change-Id: I755ba6e8b033ba29492c9ce101d1547d24c73494

--- device.mk
+++ device.mk
@@ -7,6 +7,17 @@
 # Board Platform
 TARGET_BOARD_PLATFORM := sdm845
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc \
+    $(LOCAL_PATH)/configs/idc/uinput-goodix.idc:system/usr/idc/uinput-goodix.idc
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/configs/keylayout/sdm845-tavil-snd-card_Button_Jack.kl:system/usr/keylayout/sdm845-tavil-snd-card_Button_Jack.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
 # Kernel
 TARGET_KERNEL_VERSION := 4.9
 
