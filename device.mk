c3e9a5f58d23d78742a07cbd8204ad5a6ecd48ca
Bruno Martins <bgcngm@gmail.com>
2019-03-14 23:32:40 +0000

    beryllium: Add whitelist to support exposing vendor libs
    Change-Id: I57e97048e9d1bbf7e884770de53643fb186b6204

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ TARGET_BOARD_PLATFORM := sdm845
 # Kernel
 TARGET_KERNEL_VERSION := 4.9
 
+# Native libraries whitelist
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
