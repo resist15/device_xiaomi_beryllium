4ba1844add784d721abead021daa638834b21e5a
resist15 <sourav@project404.xyz>
2023-01-25 22:35:21 +0530

    beryllium: Initial AOSPA Device Tree
    - Xiaomi POCO F1
    
    - Thanx to sdm660, lisa tree, surya tree
    
    Change-Id: I0f52b195dec685847e57368f5eae7f64b9c72fb9

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter beryllium,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Board Platform
+TARGET_BOARD_PLATFORM := sdm845
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/beryllium/beryllium-vendor.mk)
