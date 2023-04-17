e399df3226d9551c675c1b31199bdaf51c920ac4
Georg Veichtlbauer <georg@vware.at>
2023-04-17 19:29:26 +0200

    beryllium: Drop debug.sf.latch_unsignaled property
    latch_unsignaled defaults to auto single layer in Android 13.
    
    Change-Id: I8d38f3257b5f6b56c0f6b79cf4fb406a47d64af8

--- device.mk
+++ device.mk
@@ -145,7 +145,6 @@ PRODUCT_SYSTEM_PROPERTIES += \
     debug.sf.early_gl_app_phase_offset_ns=15000000 \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.latch_unsignaled=0 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
